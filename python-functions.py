388a1a9ba04a91b09632c72a656c8a304de918d4
giamazonas <skibinskirenee@gmail.com>
2022-04-04 23:27:47 -0400

    add four

--- python-functions.py
+++ python-functions.py
@@ -1,19 +1,19 @@
-# 1. 
+#1. 
 
-# def sum_to(n):
-#   return n * (n +1)/2
+def sum_to(n):
+  return n * (n +1)/2
 
-# print(sum_to(6))
+print(sum_to(6))
 
-#  2. 
+#2. 
 
-# def largest(arg):
-#   arg.sort()
-#   return(arg[-1])
+def largest(arg):
+  arg.sort()
+  return(arg[-1])
 
-# print(largest([1, 2, 3, 4, 0]))
+print(largest([1, 2, 3, 4, 0]))
 
-#  3.   
+#3.   
 
 list = 'fleep floop'
 
@@ -21,4 +21,13 @@ occurrences = list.count('e')
 print(occurrences)
 
 
-#  4.
+#4.
+
+def product(*args):
+  total = 1
+  for a in args:
+    total *= a
+  return total
+
+print(product(2, 5, 5))
+

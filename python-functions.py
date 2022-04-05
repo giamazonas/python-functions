8e11d1d6d21765fdb9bdc27014da9569fc41ca56
giamazonas <skibinskirenee@gmail.com>
2022-04-04 20:39:45 -0400

    add two

--- python-functions.py
+++ python-functions.py
@@ -1,15 +1,21 @@
 # 1. 
 
-def sum_to(n):
-  return n * (n +1)/2
+# def sum_to(n):
+#   return n * (n +1)/2
 
-print(sum_to(6))
+# print(sum_to(6))
 
 #  2. 
 
+def largest(arg):
+  arg.sort()
+  return(arg[-1])
 
+print(largest([1, 2, 3, 4, 0]))
 
 #  3.   
+
+
 
 
 #  4.
